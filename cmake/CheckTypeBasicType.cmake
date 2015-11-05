7e935624bc787807df58162d177b2a9ccec2ca7d
Alexander Haase <alexander.haase@rwth-aachen.de>
2015-11-05 16:22:33 +0100

    Changes in LICENSE header.
    Added valid LICENSE file and corrected LICENSE header in CMake module files.

--- cmake/CheckTypeBasicType.cmake
+++ cmake/CheckTypeBasicType.cmake
@@ -1,16 +1,10 @@
-# This file is part of MUST (Marmot Umpire Scalable Tool)
+# This file is part of CMake-MPIhelper.
 #
 # Copyright (C)
-#   2011-2014 ZIH, Technische Universitaet Dresden, Federal Republic of Germany
-#   2011-2014 Lawrence Livermore National Laboratories, United States of America
-#   2013-2014 RWTH Aachen University, Federal Republic of Germany
+#   2015 RWTH Aachen University, Federal Republic of Germany
 #
-# See the file LICENSE.txt in the package base directory for details
+# See the file LICENSE in the package base directory for details.
 
-
-#
-# include other CMake files
-#
 include(CheckTypeSize)
 
 
